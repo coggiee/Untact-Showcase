93badbe724d249418e39d9ac3d83b6bdb8eb4b0a
Coggie (HwisikMoon) <zentechie7@gmail.com>
2024-01-14 17:05:27 +0900

    init: 프로젝트 초기 설정

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,5 @@
 'use client'
 
-import exifr from 'exifr'
-
 export default function Home() {
-  const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files![0]
-    exifr
-      .parse(file)
-      .then((output) => console.log(output))
-  }
-  return (
-    <div>
-      <input
-        type="file"
-        name="myImage"
-        accept="image/png, image/gif, image/jpeg"
-        onChange={handleChange}
-      />
-    </div>
-  )
+  return <div>Home</div>
 }
