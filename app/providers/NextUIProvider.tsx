81682af87881ea831262f2e23bb22b9e74ca0163
Coggie (HwisikMoon) <zentechie7@gmail.com>
2024-01-14 18:14:11 +0900

    init: 프로젝트 초기 설정

--- app/providers/NextUIProvider.tsx
+++ app/providers/NextUIProvider.tsx
@@ -5,7 +5,13 @@ import { useRouter } from 'next/navigation'
 
 function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter()
-  return <NextUIProvider navigate={router.push}>{children}</NextUIProvider>
+  return (
+    <NextUIProvider
+      navigate={router.push}
+      className="w-full h-screen container mx-auto px-5">
+      {children}
+    </NextUIProvider>
+  )
 }
 
 export default Providers
