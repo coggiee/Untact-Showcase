ac24ba6d3a8934d70b13020330c847acd9c7a8e3
Coggie (HwisikMoon) <zentechie7@gmail.com>
2024-01-25 17:46:39 +0900

    feat: 지도에 핀과 마커 기능 추가

--- app/components/MapBox.Container.tsx
+++ app/components/MapBox.Container.tsx
@@ -1,7 +1,10 @@
 'use client'
 
-import React from 'react'
+import React, { useMemo, useState } from 'react'
+import { Marker } from 'react-map-gl'
+import CITIES from '@/data/cities.json'
 import MapBoxPresenter from './MapBox.Presenter'
+import Pin from './Pin'
 
 function MapBoxContainer() {
   const [viewState, setViewState] = React.useState({
@@ -12,22 +15,49 @@ function MapBoxContainer() {
     pitch: 0
   })
 
+  const [popupInfo, setPopupInfo] = useState<{
+    longitude: number
+    latitude: number
+  } | null>(null)
+
+  const pins = useMemo(
+    () =>
+      CITIES.map((city) => (
+        <Marker
+          key={`marker-${city.longitude}`}
+          longitude={city.longitude}
+          latitude={city.latitude}
+          anchor="bottom"
+          onClick={(e) => {
+            e.originalEvent.stopPropagation()
+            setPopupInfo(city)
+          }}>
+          <Pin size={20} />
+        </Marker>
+      )),
+    []
+  )
+
   const handleMoveMap = (event: any) => {
     setViewState(event.viewState)
   }
 
   const handleClickMap = (event: any) => {
     const { lat, lng } = event.lngLat
-
     console.log(lat, lng)
   }
 
   return (
-    <MapBoxPresenter
-      viewState={viewState}
-      onClickMap={handleClickMap}
-      onMoveMap={handleMoveMap}
-    />
+    <section className="w-full h-3/5">
+      <MapBoxPresenter
+        viewState={viewState}
+        onClickMap={handleClickMap}
+        onMoveMap={handleMoveMap}
+        pins={pins}
+        popupInfo={popupInfo}
+        onClickPopup={() => setPopupInfo(null)}
+      />
+    </section>
   )
 }
 
--- app/components/MapBox.Presenter.tsx
+++ app/components/MapBox.Presenter.tsx
@@ -1,30 +1,51 @@
 'use client'
 
 import React from 'react'
-import Map from 'react-map-gl'
+import Map, { Popup } from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
 interface MapBoxPresenterProps {
   viewState: any
   onClickMap: (event: any) => void
   onMoveMap: (event: any) => void
+  onClickPopup: () => void
+  pins: any
+  popupInfo: any
 }
 
 function MapBoxPresenter({
   viewState,
   onClickMap,
-  onMoveMap
+  onMoveMap,
+  onClickPopup,
+  pins,
+  popupInfo
 }: MapBoxPresenterProps) {
   return (
     <Map
       {...viewState}
       reuseMaps
-      style={{ width: '100%', height: 400, marginTop: 20 }}
+      style={{ width: '100%', height: '100%', marginTop: 20, borderRadius: 10 }}
       mapStyle="mapbox://styles/mapbox/dark-v9"
       mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
       onClick={onClickMap}
       onMove={onMoveMap}
-    />
+      scrollZoom={false}>
+      {pins}
+
+      {popupInfo && (
+        <Popup
+          anchor="top"
+          longitude={Number(popupInfo.longitude)}
+          latitude={Number(popupInfo.latitude)}
+          onClose={onClickPopup}>
+          <div>
+            <span>{Number(popupInfo.longitude)}</span>
+            <span>{Number(popupInfo.latitude)}</span>
+          </div>
+        </Popup>
+      )}
+    </Map>
   )
 }
 
