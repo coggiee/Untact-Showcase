41e4cba75d7a8296570557ced86663893410f124
lunarmoon7 <zentechie7@gmail.com>
2024-04-10 22:04:46 +0900

    style: 랜딩 페이지 스타일 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
-'use client'
+import Footer from './components/Footer'
+import LandingSection from './components/LandingSection'
+import MapSection from './components/MapSection'
 
 export default function Home() {
-  return <div>Home</div>
+  return (
+    <div className="w-full h-full snap-y snap-mandatory overflow-y-scroll scroll-py-10  scrollbar-hide">
+      <LandingSection />
+      <MapSection />
+      <Footer />
+    </div>
+  )
 }
